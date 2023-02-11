8c0b9ceac1090de0217cb586a9d86dfbdbc820f7
Kichkinev Vladislav <100088832+KIchkinevVladislav@users.noreply.github.com>
2023-02-11 17:27:48 +0300

    Update models.py

--- image_db/api/models.py
+++ image_db/api/models.py
@@ -1,6 +1,7 @@
 from django.db import models
 from django.contrib.auth.models import AbstractUser
 
+
 class User(AbstractUser):
     """
     User Model
@@ -27,6 +28,7 @@ class User(AbstractUser):
     USERNAME_FIELD = 'email'
     REQUIRED_FIELDS = ('username',)
 
+    
     class Meta:
         verbose_name = 'Пользователь'
         verbose_name_plural = 'Пользователи'
